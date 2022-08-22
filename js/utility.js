efc0ac94bc2e0a48450f30324f318f9445a18d65
Md. Saiful Islam <saiful9122113@gmail.com>
2022-08-22 16:40:11 +0600

    EventListener are added on to the selected Players

--- js/utility.js
+++ js/utility.js
@@ -10,3 +10,24 @@ function getFloatInnerText(id) {
 function setValue(id) {
     return document.getElementById(id)
 }
+
+function getInnerText(id) {
+    return document.getElementById(id).innerText;
+}
+
+function selectPlayer(btnId, nameId) {
+    document.getElementById(btnId).addEventListener('click', function () {
+        const name = getInnerText(nameId);
+
+        const newTag = document.createElement('li');
+        newTag.textContent = name;
+        ol.appendChild(newTag);
+
+        const btnEnable = document.getElementById(btnId);
+        btnEnable.setAttribute('disabled', true);
+
+        var item = document.getElementById(btnId);
+        item.setAttribute("style", "background-color:gray;");
+    });
+
+}
